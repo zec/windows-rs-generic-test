8bc35a6111db527e6962935d6090c5d98900515f
Zachary Catlin <z@zc.is>
2021-03-10 15:01:09 -0500

    Started an implementation of IIterator<HString>

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
 fn main() {
     windows::build!(
         windows::foundation::IStringable,
+        windows::foundation::collections::IIterable,
+        windows::foundation::collections::IIterator,
+
+        windows::win32::debug::SetLastError,
+        windows::win32::system_services::E_BOUNDS,
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ extern crate windows;
 mod bindings {
     ::windows::include_bindings!();
 
+    use std::cmp::min;
     use std::sync::Arc;
-    use ::windows::{HString, Error};
+    use ::windows::{HString, Error, ErrorCode};
+    use self::windows::win32::debug::SetLastError;
+    use self::windows::win32::system_services::E_BOUNDS;
 
     #[::windows::implement(windows::foundation::IStringable)]
     pub struct TestStringable {
@@ -16,6 +19,47 @@ mod bindings {
     impl TestStringable {
         pub fn to_string(&self) -> Result<HString, Error> {
             Ok("aaah.".into())
+        }
+    }
+
+    struct TestIterator {
+        v: Arc<Vec<HString>>,
+        idx: usize,
+    }
+
+    // IIterator impl
+    impl TestIterator {
+        fn current(&self) -> Result<HString, Error> {
+            if self.idx < self.v.len() {
+                Ok(self.v[self.idx].clone())
+            } else {
+                unsafe { SetLastError(E_BOUNDS as u32); }
+                Err(ErrorCode::from_thread().into())
+            }
+        }
+
+        fn has_current(&self) -> Result<bool, Error> {
+            Ok(self.idx < self.v.len())
+        }
+
+        fn move_next(&mut self) -> Result<bool, Error> {
+            if self.idx >= self.v.len() {
+                unsafe { SetLastError(E_BOUNDS as u32); }
+                return Err(ErrorCode::from_thread().into())
+            }
+
+            self.idx += 1;
+            Ok(self.idx < self.v.len())
+        }
+
+        fn get_many(&mut self, items: &mut [HString]) -> Result<u32, Error> {
+            let base = self.idx;
+            let num_to_copy = min(self.v.len() - base, items.len());
+            for i in 0..num_to_copy {
+                items[i] = self.v[base + i].clone();
+            }
+            self.idx += num_to_copy;
+            Ok(num_to_copy as u32)
         }
     }
 }
