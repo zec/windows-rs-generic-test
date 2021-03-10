689428caa2618c9216846f66788b2a0457723e68
Zachary Catlin <z@zc.is>
2021-03-10 13:40:56 -0500

    Added implementation of IStringable

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,21 @@ extern crate windows;
 #[allow(dead_code)]
 mod bindings {
     ::windows::include_bindings!();
+
+    use std::sync::Arc;
+    use ::windows::{HString, Error};
+
+    #[::windows::implement(windows::foundation::IStringable)]
+    pub struct TestStringable {
+        pub x: u8,
+    }
+
+    // IStringable impl
+    impl TestStringable {
+        pub fn to_string(&self) -> Result<HString, Error> {
+            Ok("aaah.".into())
+        }
+    }
 }
 
 fn main() {
